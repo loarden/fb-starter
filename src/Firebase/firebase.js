37f7a6a53539088492aa9fb0b85f427c658505b2
loarden <biacsi.ors@gmail.com>
2023-05-16 16:29:16 +0200

    deleted config from firebase

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAMS13qxjbyJHczN96__abxBj4gRv4SbXs",
-  authDomain: "brewitter-e178c.firebaseapp.com",
-  projectId: "brewitter-e178c",
-  storageBucket: "brewitter-e178c.appspot.com",
-  messagingSenderId: "893480219680",
-  appId: "1:893480219680:web:2a4568a78efcce360fb845",
-  measurementId: "G-4L59B31310"
+  //your config...
 };
 
 // Initialize Firebase
